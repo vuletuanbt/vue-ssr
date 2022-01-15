7bb4c2456faf7974f709cc7be4a1c5060895e61f
vuletuan <vult@zigexn..vn>
2022-01-15 15:10:33 +0700

    set up inertiajs

---
+++ resources/js/Pages/About.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>Inertiajs is {{ about_us }}</h1>
+
+        <Link href="/">Go to Home</Link>
+    </div>
+</template>
+
+<script>
+import {Link} from "@inertiajs/inertia-vue3";
+
+export default {
+    components: {
+        Link,
+    },
+    props: ["about_us"],
+    created() {
+        console.log(this.about_us)
+    }
+};
+</script>
